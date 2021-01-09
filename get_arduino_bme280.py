a03e9c375ff4c091516722905831a4fd8243545b
Artem Chirtsov <chirartem@gmail.com>
2021-01-09 16:43:56 +0300

    first commit

---
+++ get_arduino_bme280.py
@@ -0,0 +1,16 @@
+import serial
+import time
+
+device = '/dev/ttyACM0'
+
+try:
+    print("Trying...", device)
+    arduino = serial.Serial(device, 115200)
+except:
+    print("failed to connect on", device)
+
+while True:
+    time.sleep(0.1)
+    data = arduino.readline()
+    print(data.decode('utf-8'))
+
